7460c0e798e58d0fdcfb9d959c8ae4bd15cf596e
Leidy-dii <leidysan0893@gmail.com>
2023-04-08 19:20:15 -0600

    first commit

---
+++ src/componentes/Footer/index.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css"
+
+const Footer = () => {
+    return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)"}}>
+        
+        <div className='redes'>
+            <a href='https://www.aluracursos.com/'></a>
+            
+            <a href='https://www.aluracursos.com/'></a>
+            
+            <a href='https://www.aluracursos.com/'></a>   
+        </div>
+    </footer>
+}
+
+export default Footer
